b9153838a4e143da93297735754db67c912850fe
MD SRK <144815032+Eight22@users.noreply.github.com>
2023-10-15 13:11:25 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -219,7 +219,7 @@ Tɪᴘ: Sᴇʟᴇᴄᴛ "Lᴀɴɢᴜᴀɢᴇs" ᴏʀ "Sᴇᴀsᴏɴs" Bᴜᴛᴛ
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂:- {file_name}
+    CAPTION = """<b>📂 {file_name}
 <b>♻️ Size: {file_size}</b>"""
     IMDB_TEMPLATE_TXT = """
 <b>✅ I Found: {qurey}
